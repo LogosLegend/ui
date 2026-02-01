a869777fc5953a5c43a9451e24e16c36459f15a2
LogosLegend <75035186+LogosLegend@users.noreply.github.com>
2026-02-01 17:27:31 +0600

    chore: rename variables and add second key

--- .github/scripts/sign-status.mjs
+++ .github/scripts/sign-status.mjs
@@ -1,19 +1,22 @@
 import { readFileSync, writeFileSync } from 'node:fs';
 import { sign } from 'node:crypto';
 
-const PRIVATE_KEY = readFileSync(process.env.PRIVATE_KEY_PATH, 'utf8');
-
 const statusData = {
   status: "active",
   timestamp: Date.now(),
 }
 
 const payload = JSON.stringify(statusData, null, 0);
-const signature = sign(null, Buffer.from(payload), PRIVATE_KEY).toString('base64');
+
+const [signature, uniSignature] = [process.env.APP_PRIVATE_KEY_PATH, process.env.UNI_PRIVATE_KEY_PATH].map(path =>
+  sign(null, Buffer.from(payload), readFileSync(path, 'utf8'))
+  .toString('base64')
+);
 
 const signedStatus = {
   ...statusData,
-  signature
+  signature,
+  uniSignature,
 }
 
 writeFileSync('status.json', JSON.stringify(signedStatus, null, 2));
